5a07d217e3e880324e0fb43f225ee6dd55663469
Lucas Dantas <95001459+lucassdantas@users.noreply.github.com>
2022-05-15 16:50:26 -0300

    better responsive nav

--- script.js
+++ script.js
@@ -1,15 +1,26 @@
 let navButton = document.querySelector("#cellButton")
+let cellFundo = document.querySelector(".cell_fundo")
+
 
 function Clique(){
     let cellLogin = document.querySelector("#login_li").classList
     let cellMenu = document.querySelector("#nav_menu").classList
+    let cellFundo = document.querySelector(".cell_fundo")
     if(cellLogin.value.indexOf("escondido") <= -1){
         cellLogin.add("escondido")
         cellMenu.add("escondido")
+        cellFundo.classList.remove("active")
     } else{
         cellLogin.remove("escondido")
         cellMenu.remove("escondido")
+        cellFundo.classList.add("active")
     }
-
 }
 navButton.addEventListener("click", Clique)
+cellFundo.addEventListener("click", Clique)
+
+let Corpo = document.querySelector("#corpo")
+
+
+
+
