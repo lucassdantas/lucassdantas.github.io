f9a15c693cff057d3b654f80e03b92dcca14cdec
Lucas Dantas <95001459+lucassdantas@users.noreply.github.com>
2022-05-25 19:15:10 -0300

    fix responsive

---
+++ Database/Database.sql
@@ -0,0 +1,16 @@
+create database if not exists usuarios default character set utf8 default collate utf8_general_ci;
+use usuarios;
+create table if not exists usuarios (
+	id int not null auto_increment,
+	nome varchar(50) not null,
+	sexo enum("F","M"),
+	email varchar(120),
+    nascimento date,
+	primary key(id)
+) default charset = utf8;
+
+insert into usuarios values
+	(default, "Lucas", "M", "lucasdesousadantas@gmail.com","2002-01-07"),
+    (default, "Admin", "M", "lucasdantasprogramador@gmail.com","2002-01-07");
+    
+    
