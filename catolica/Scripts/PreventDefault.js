fbe28671088b6d4c54376e59f535fdb513789cb4
Lucas Dantas <95001459+lucassdantas@users.noreply.github.com>
2022-06-04 20:56:15 -0300

    Delete catolica directory

--- catolica/Scripts/PreventDefault.js
+++
@@ -1,7 +0,0 @@
-let BtnForm = document.querySelector("#btn-login")
-function noDefault(){
-    let form = document.querySelector("#formulario")
-    form.preventDefault()
-    console.log(form)
-}
-BtnForm.addEventListener("click", noDefault)
