3586a0cc526dc4560062ccb26655e4d8acb2c0eb
Lucas Dantas <95001459+lucassdantas@users.noreply.github.com>
2022-06-04 21:16:23 -0300

    Delete softwares/BreakLinveRemover directory

--- softwares/BreakLinveRemover/script.js
+++
@@ -1,7 +0,0 @@
-let botao = document.querySelector("#Button")
-botao.addEventListener("click", function(){
-    let text = document.querySelector("#Text").value
-    let resposta = document.querySelector("#Answer")
-    let formatacao = text.replace(/\n/g, ", ")
-    resposta.value=(formatacao)
-})
