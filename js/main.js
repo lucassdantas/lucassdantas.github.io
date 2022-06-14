564a87f7f5be51d77217fd6f7c936762d9908229
Lucas Dantas <95001459+lucassdantas@users.noreply.github.com>
2022-06-14 19:34:59 -0300

    add about, experiences and navbar

---
+++ js/main.js
@@ -0,0 +1,18 @@
+/*Menu dropwdown */
+document.addEventListener('DOMContentLoaded', function() {
+    let elems = document.querySelectorAll('.dropdown-trigger');
+    let instances = M.Dropdown.init(elems, {
+        isScrollable: false,
+        hover: true,
+        coverTrigger: false,
+        constrainWidth: true,
+    });
+  });
+
+/*Menu para celular*/ 
+document.addEventListener('DOMContentLoaded', function() {
+    let elems = document.querySelectorAll('.sidenav');
+    let instances = M.Sidenav.init(elems, {
+        draggable: true
+    });
+  });
