de8b6eff6c648ef97d316dc3286361fd5536c9ec
Lucas Dantas <lucasdesousadantas@gmail.com>
2022-10-22 08:00:32 -0300

    add rd experience

--- js/main.js
+++ js/main.js
@@ -30,6 +30,7 @@ document.addEventListener('DOMContentLoaded', function() {
 document.addEventListener('DOMContentLoaded', function() {
   var elems = document.querySelectorAll('.sidenav');
   var instances = M.Sidenav.init(elems, {
+    draggable:true,
     
   });
 });
