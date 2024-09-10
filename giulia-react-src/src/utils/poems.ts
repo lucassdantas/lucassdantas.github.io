1b0b9cef96822f365a7a4683c553b6259a06a0f4
Lucas Dantas <lucasdantasprogramador@gmail.com>
2024-09-10 00:28:12 -0300

    change poem orders

--- giulia-react-src/src/utils/poems.ts
+++ giulia-react-src/src/utils/poems.ts
@@ -6,31 +6,6 @@ export type Poem = {
 
 export const poems:Poem[] = [
   {
-    date:'',
-    title:'A sua luz',
-    stanzas:[
-      [
-        'o passaro canta,mas beleza pouco produz',
-        'a flor desabrocha,mas beleza pouco reluz',
-        'o sol ilumina,mas beleza pouco entreluz',
-        'o dia começa,mas cadê a beleza, meu Jesus?',
-      ],
-      [
-        'Após esta indagação, aos meus olhos, Ele apresenta uma luz',
-      ],
-      [
-      'É no canto da sua voz, que a beleza muito produz',
-      'É no romper do seu sorrir, que a beleza muito reluz',
-      'É no brilho do seu olhar, que a beleza muito conduz',
-      'É assim que o dia começa, que encontro a beleza, meu Jesus!',
-      ],
-      [
-      'Esta luz é a Giulia, a beleza mais certa,',
-      'que faz da vida mais bela, que a Jesus me conduz.'
-      ],
-    ]
-  },
-  {
     date:'22/06/2024',
     title: 'Seu valor',
     stanzas: [
@@ -210,6 +185,31 @@ export const poems:Poem[] = [
         'Guiou-me na mesma e depois iluminou nossa razão,',
         'Que o Espírito Santo abençoe e una nosso coração!'
       ]
+    ]
+  },
+  {
+    date:'',
+    title:'A sua luz',
+    stanzas:[
+      [
+        'o passaro canta,mas beleza pouco produz',
+        'a flor desabrocha,mas beleza pouco reluz',
+        'o sol ilumina,mas beleza pouco entreluz',
+        'o dia começa,mas cadê a beleza, meu Jesus?',
+      ],
+      [
+        'Após esta indagação, aos meus olhos, Ele apresenta uma luz',
+      ],
+      [
+      'É no canto da sua voz, que a beleza muito produz',
+      'É no romper do seu sorrir, que a beleza muito reluz',
+      'É no brilho do seu olhar, que a beleza muito conduz',
+      'É assim que o dia começa, que encontro a beleza, meu Jesus!',
+      ],
+      [
+      'Esta luz é a Giulia, a beleza mais certa,',
+      'que faz da vida mais bela, que a Jesus me conduz.'
+      ],
     ]
   },
   
