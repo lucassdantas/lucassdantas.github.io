d2c881cc14b727b2740bd7bf9f607a5cf4b7c823
Lucas Dantas <lucasdantasprogramador@gmail.com>
2024-09-10 00:20:19 -0300

    fix mime error

--- giulia-react-src/src/components/Card/index.tsx
+++ giulia-react-src/src/components/Card/index.tsx
@@ -1,9 +1,7 @@
-import React from 'react'
-import flowerImg from '@/assets/giulia.jpg'
 export const Card = () => {
   return (
     <div className='rounded-lg relative flex flex-col justify-center items-center p-4 shadow-md bg-white mb-12 w-fit'>
-      <img src={flowerImg} className='rounded-full w-[120px] h-[120px]'/>
+      <img src='https://lucassdantas.github.io/giulia/giulia.jpg' className='rounded-full w-[120px] h-[120px]'/>
       <h3 className="font-bold my-2 text-lg">Giulia Lima</h3>
         <p>A beleza mais bela</p>
         <p>Que me conduz a Jesus</p>
