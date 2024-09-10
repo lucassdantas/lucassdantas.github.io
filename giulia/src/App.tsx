6a669a64f4aea66c6d801f13fdfaad849839e28b
Lucas Dantas <lucasdantasprogramador@gmail.com>
2024-09-09 23:35:57 -0300

    adding poems

--- giulia/src/App.tsx
+++ giulia/src/App.tsx
@@ -7,11 +7,11 @@ import './App.css';
 
 function App() {
   return (
-    <main className="min-h-screen h-fit bg-gradient-to-br from-pink-300 to-pink-400 text-black">
+    <main className="min-h-screen h-fit bg-gradient-to-br from-pink-300 to-pink-400 text-black py-8">
       <Limiter>
         <Card />
         <Carousel>
-          {poems.map((poem: Poem, i: number) => <PoemStructure title={poem.title} stanzas={poem.stanzas} key={i} />)}
+          {poems.map((poem: Poem, i: number) => <PoemStructure date={poem.date} title={poem.title} stanzas={poem.stanzas} key={i} />)}
         </Carousel>
       </Limiter>
     </main>
--- giulia/src/utils/poems.ts
+++ giulia/src/utils/poems.ts
@@ -1,10 +1,12 @@
 export type Poem = {
   title: string;
   stanzas: string[][];
+  date:string;
 };
 
 export const poems:Poem[] = [
   {
+    date:'',
     title:'A sua luz',
     stanzas:[
       [
@@ -29,11 +31,164 @@ export const poems:Poem[] = [
     ]
   },
   {
-    title:'Outro poema',
-    stanzas:[
+    date:'22/06/2024',
+    title: 'Seu valor',
+    stanzas: [
       [
-        'outro poema'
+        'A felicidade de quem é teu amigo,',
+        'A união que dá aos que estão contigo,'
+      ],
+      [
+        'O seu empenho em todo estudo,',
+        'O valor que dás ao bom conteúdo,'
+      ],
+      [
+        'A dedicação que tem a Igreja,',
+        'A proximidade que Dela deseja,'
+      ],
+      [
+        'Todas são qualidades que vem de você,',
+        'Que te dão o valor que tens mas não vê.',
+        'Que compõem suas virtudes, que vem do seu ser.',
+        'Este que Deus sempre sonhou em criar e com ele viver.'
+      ],
+      [
+        'Ser independente e valioso que escolhi amar,',
+        'Com a ajuda de Deus, neste amor hei de me aprofundar.'
+      ],
+      [
+        'Vós, igualmente, um amor escolheste,',
+        'Espero que seja bom quem recebeste.'
+      ],
+      [
+        'Há um mês iniciamos essa jornada,',
+        'Que Deus nos conduza nessa estrada.'
+      ],
+      [
+        'Com Ele, a Ele caminharemos,',
+        'Amando-O, um ao outro mais amaremos.'
       ]
     ]
-  }
+  },
+  {
+    date:'22/08/2024',
+    title: 'Mais bela que todo conhecimento',
+    stanzas: [
+
+      [
+        'Uma estrela a cintilar;',
+        'O céu, observar;',
+        'O dia a clarear;',
+        'A ave a voar;'
+      ],
+      [
+        'O ser, a viver;',
+        'A natureza, conhecer;',
+        'Os problemas, resolver;',
+        'As coisas, aprender;'
+      ],
+      [
+        'Desses deleites e desejos,',
+        'O maior de todos não foi citado.',
+        'Ele é o maior dos ensejos:',
+        'É estar sempre ao seu lado.'
+      ],
+      [
+        'O brilho da sua beleza,',
+        'Sobrepuja o de todo astro.',
+        'Na sua, há maior profundeza;',
+        'E é sua que tenho amado.'
+      ],
+      [
+        'Contigo, sempre é claro',
+        'Pois me dás muita alegria;',
+        'Seu amor, me é muito caro,',
+        'Sua animação me contagia.'
+      ],
+      [
+        'Às nuvens, seu sorriso me leva;',
+        'Do Céu, você me aproxima;',
+        'Por você, meu amor se eleva;',
+        'Por você, tenho muita estima.'
+      ],
+      [
+        'Como seria viver sem você?',
+        'Seria triste não te conhecer',
+        'Sem você, não haveria porquê,',
+        'Com você, alegre é meu viver.'
+      ],
+      [
+        'A natureza da tua humanidade',
+        'É mais bela que toda paisagem:',
+        'Não há uma que tenha igualdade',
+        'Não há uma com tão bela mensagem.'
+      ],
+      [
+        'Os problemas são tantos',
+        'Mas o amor vence a tudo.',
+        'Mais forte que encantos,',
+        'Ele supera, mesmo que mudo.'
+      ],
+      [
+        'De todo aprendizado,',
+        'O seu é o mais valioso.',
+        'De tudo que tenho lido,',
+        'Sois o livro mais valoroso.'
+      ],
+      [
+        'Como já dizia o santo:',
+        'A santidade é amar muito.',
+        'De que serve saber tanto',
+        'Se, de amor, não há intuito?'
+      ],
+      [
+        'Amar-te mais, é o que desejo.',
+        'Teu amor, vou corresponder.',
+        'Contigo, a santidade é o que almejo.',
+        'Que nosso amor mútuo esteja sempre a crescer.'
+      ]
+    ]
+  },
+  {
+    title: 'No silêncio do meu labor',
+    date: '25/04/2024',
+    stanzas: [
+      [
+        'No silêncio do meu labor,',
+        'Cristo, através de ti, dá-me o vigor.'
+      ],
+      [
+        'Colocando-a em minha vida',
+        'Cristo, de minhas preces, fez desta concedida:'
+      ],
+      [
+        '"Mostra-me o caminho, mostra-me o trajeto',
+        'Pois, qual percurso seguir, não tenho por certo."'
+      ],
+      [
+        'Lembro-me de mais uma especial',
+        'Cristo, a essa prece, está atendendo sem igual:'
+      ],
+      [
+        '"Ensina-me o sentido, ensina-me o sacrifício,',
+        'Pois, do amor, pouco ou nada tenho para exercício."'
+      ],
+      [
+        'Eis que esforço-me pela carreira',
+        'Cristo, por você, inspira-me a entregar-me pela família inteira;'
+      ],
+      [
+        'Eis que esforço-me pelo estudo',
+        'Pois, por Cristo e você, tenho o interesse por todo conteúdo;'
+      ],
+      [
+        'Eis que esforço-me pela prudência',
+        'Cristo, por você, faz tudo no tempo da divina providência;'
+      ],
+      [
+        'É assim que, no silêncio do meu labor,',
+        'Cristo, através de ti, dá-me o vigor.'
+      ]
+    ]
+  },
 ]
